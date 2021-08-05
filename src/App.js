efa6bc8b1277dd1d64e03ef0ed0b0717a805dce2
khasanberdiev <69322046+khasanberdiev@users.noreply.github.com>
2021-08-05 07:00:17 -0700

    rr rr

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandfbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello ddddd CodeSandfbox</h1>
+      <h2>Start jkj editing to see some magic happen!</h2>
     </div>
   );
 }
